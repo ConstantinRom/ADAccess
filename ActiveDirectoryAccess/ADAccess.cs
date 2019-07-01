0760f7d53e01c4b62f8e476e160763bf41ad69dd
Constantin Rom <constantin-rom@gmx.de>
2019-07-01 11:49:51 +0200

    Created Project ActiveDiretoryAccess

---
+++ ActiveDirectoryAccess/ADAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryAccess
+{
+    public class Class1
+    {
+    }
+}
